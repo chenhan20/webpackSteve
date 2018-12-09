1695726257308d2432344a87e535c1075fb42810
SteveChuang <a0911558945@gmail.com>
2018-12-10 00:05:05 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app:'./src/index.js',
     print:'./src/print.js'
   },
+    devtool: 'inline-source-map',
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
           title: 'Output Management'
         })
